4599e2f7e628a06f430b8ea9002bb4eb09a30c1a
'alonsogr03' <'alonsogonro@gmail.com'>
2024-12-31 13:20:12 +0100

    Clase Hamburguesería bien. Clase Ticket falta retocar el último método.

--- BurgerKingJuanCarlos/src/burgerkingjuancarlos/Hamburgueseria.java
+++ BurgerKingJuanCarlos/src/burgerkingjuancarlos/Hamburgueseria.java
@@ -13,50 +13,6 @@ public class Hamburgueseria implements ModoCliente,ModoAdministrador,Serializabl
     CatalogoProductos catalogo = new CatalogoProductos();
     ArrayList<Ticket> listadoTickets = new ArrayList<>();
     private Ticket pedido;
-    
-   
-   
-     // Método para realizar un pedido
-    public void realizarPedido() {
-
-        Scanner scanner = new Scanner(System.in);
-        int longitudListaTickets=listadoTickets.size();
-        Ticket ticket = new Ticket(longitudListaTickets+1);
-        int tiempoTotal = 0;
-
-        System.out.println("--- Realizar Pedido ---");
-        while (true) {
-            System.out.println("\nProductos disponibles:");
-            for (int i = 0; catalogo.tamañoCatalogo(productospedidos)>= i; i++) {
-                Producto producto = catalogo.getProductos().get(i);
-                System.out.printf("%d. %s - Precio: %d céntimos, Tiempo: %d segundos\n",
-                        i + 1, producto.getNombre(), producto.getPrecio(), producto.getTiempoElaboracion());
-            }
-
-            System.out.println("0. Finalizar pedido");
-            System.out.print("Seleccione el número del producto que desea agregar: ");
-            int opcion = scanner.nextInt();
-
-            if (opcion == 0) {
-                break;
-            }
-
-            if (opcion > 0 && opcion <= catalogo.getProductos().size()) {
-                Producto productoSeleccionado = catalogo.getProductos().get(opcion - 1);
-                int cantidad=0;
-               
-                ticket.añadirProducto(productoSeleccionado,cantidad);
-                tiempoTotal += productoSeleccionado.getTiempoElaboracion();
-                System.out.printf("%s agregado al pedido.\n", productoSeleccionado.getNombre());
-            } else {
-                System.out.println("Opción no válida, por favor intente de nuevo.");
-            }
-        }
-        pedido.setTiempoEstimado(tiempoTotal);
-        listadoTickets.add(pedido);
-        System.out.printf("Pedido finalizado. Tiempo estimado: %d segundos, Precio total: %d céntimos\n",
-                ticket.getTiempoEstimado(), ticket.getPrecioTotal());
-    }
 
     public CatalogoProductos getCatalogo() {
         return catalogo;
@@ -66,8 +22,9 @@ public class Hamburgueseria implements ModoCliente,ModoAdministrador,Serializabl
         this.catalogo = catalogo;
     }
 
-   
-
+    public int longitudTickets(){
+        return listadoTickets.size();
+    }
     public ArrayList<Ticket> getListadoTickets() {
         return listadoTickets;
     }
--- BurgerKingJuanCarlos/src/burgerkingjuancarlos/Ticket.java
+++ BurgerKingJuanCarlos/src/burgerkingjuancarlos/Ticket.java
@@ -4,8 +4,10 @@
  */
 package burgerkingjuancarlos;
 
+import static burgerkingjuancarlos.ModoCliente.productospedidos;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Ticket {
     private int id;
@@ -83,6 +85,50 @@ public class Ticket {
 
     public void setHoraPedido(LocalDateTime horaPedido) {
         this.horaPedido = horaPedido;
+    }
+    
+    
+    
+     // Método para realizar un pedido
+    public void realizarPedido() {
+
+        Scanner scanner = new Scanner(System.in);
+        int longitudListaTickets=listadoTickets.size();
+        Ticket ticket = new Ticket(longitudListaTickets+1);
+        int tiempoTotal = 0;
+
+        System.out.println("--- Realizar Pedido ---");
+        while (true) {
+            System.out.println("\nProductos disponibles:");
+            for (int i = 0; catalogo.tamañoCatalogo(productospedidos)>= i; i++) {
+                Producto producto = catalogo.getProductos().get(i);
+                System.out.printf("%d. %s - Precio: %d céntimos, Tiempo: %d segundos\n",
+                        i + 1, producto.getNombre(), producto.getPrecio(), producto.getTiempoElaboracion());
+            }
+
+            System.out.println("0. Finalizar pedido");
+            System.out.print("Seleccione el número del producto que desea agregar: ");
+            int opcion = scanner.nextInt();
+
+            if (opcion == 0) {
+                break;
+            }
+
+            if (opcion > 0 && opcion <= catalogo.getProductos().size()) {
+                Producto productoSeleccionado = catalogo.getProductos().get(opcion - 1);
+                int cantidad=0;
+               
+                ticket.añadirProducto(productoSeleccionado,cantidad);
+                tiempoTotal += productoSeleccionado.getTiempoElaboracion();
+                System.out.printf("%s agregado al pedido.\n", productoSeleccionado.getNombre());
+            } else {
+                System.out.println("Opción no válida, por favor intente de nuevo.");
+            }
+        }
+        pedido.setTiempoEstimado(tiempoTotal);
+        listadoTickets.add(pedido);
+        System.out.printf("Pedido finalizado. Tiempo estimado: %d segundos, Precio total: %d céntimos\n",
+                ticket.getTiempoEstimado(), ticket.getPrecioTotal());
     }
 
    
